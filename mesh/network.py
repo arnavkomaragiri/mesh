218fa23401a0cf60ce739d709a69734509626427
Arnav Komaragiri <komaraac@mail.uc.edu>
2023-09-03 00:47:30 -0400

    finished basic init/load/save/add functionality, need to implement search and fix load bug

--- mesh/network.py
+++ mesh/network.py
@@ -16,7 +16,6 @@ from pymilvus import (
 from cohere_handler import *
 from typing import Dict
 
-
 Network = Dict[str, Union[Dict, np.array, nx.Graph]]
 
 def get_entity_summary(client: cohere.Client, related: List[Dict], model: str, num_tokens: int, temperature: float, **kwargs) -> str:
@@ -26,14 +25,16 @@ def get_entity_summary(client: cohere.Client, related: List[Dict], model: str, n
              "Capture the core concepts of the specified content while avoiding unnecessary words",
              "Use the specified context to inform the summarization process; the content given exists within a larger graph structure",
              "The <DOCUMENT> tag corresponds to where the specified content exists within the larger data structure"]
-    context_str = reduce(lambda a, b: f'{a}\n{b}', [f"- {entity['summary']}" for entity in related if 'summary' in entity])
+    context_str = None
+    if len(related) > 0:
+        context_str = reduce(lambda a, b: f'{a}\n{b}', [f"- {entity['summary']}" for entity in related if 'summary' in entity])
     return query_generate(client, model, num_tokens, temperature, 
                           prompt=prompt, rules=rules, context_str=context_str, output_name="Summary", **kwargs)
 
 def init_network(api_key: str, host: str, port: int, alias: str, dim: int, collection_str: str = "data") -> Network:
     # connect to milvus and check if we're making a new collection
     connections.connect(alias, host=host, port=port)
-    if utility.has_collection(collection):
+    if utility.has_collection(collection_str, using=alias):
         raise ValueError(f"collection {collection_str} already exists, cannot build network")
 
     # build vector database schema
@@ -42,7 +43,7 @@ def init_network(api_key: str, host: str, port: int, alias: str, dim: int, colle
         FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=dim)
     ]
     schema = CollectionSchema(fields, "mesh vector database store")
-    collection = Collection(collection_str, schema)
+    collection = Collection(collection_str, schema, using=alias)
 
     network = {
         "milvus_info": {
@@ -54,10 +55,10 @@ def init_network(api_key: str, host: str, port: int, alias: str, dim: int, colle
         },
         "client": init_client(api_key),
         "graph": nx.Graph(),
-        "id_map": Dict[int, Union[int, str]]
+        "id_map": {},
     }
     # load collection
-    network['milvus_info']['collection'].load()
+    # network['milvus_info']['collection'].load()
     return network
 
 def open_network(api_key: str, path: str) -> Network:
@@ -70,7 +71,6 @@ def open_network(api_key: str, path: str) -> Network:
     milvus_info = network['milvus_info']
     connections.connect(alias=milvus_info['alias'], host=milvus_info['host'], port=milvus_info['port']) 
     network['milvus_info']['collection'] = Collection(milvus_info['collection_str'])
-    network['milvus_info']['collection'].load()
 
     # rebuild local graph from node link data
     network['graph'] = nx.node_link_graph(network['graph'])
@@ -90,14 +90,11 @@ def close_network(network: Network, out_path: str):
     with open(out_path, 'w') as f:
         json.dump(network, f)
 
-def add_entity(network: Network, node_id: Union[str, int], connected: List[Any], content: str, 
+def add_entity(network: Network, node_id: Union[str, int], content: str, connected: List[Any] = [],
                file_path: Optional[str] = None) -> Network:
     if node_id in network['graph']:
         raise ValueError(f"entity {node_id} already exists in graph")
     network['graph'].add_node(node_id)
-
-    # create new entry in id map
-    network['id_map'][hash(node_id)] = node_id
 
     # set preliminary metadata
     network['graph'].nodes[node_id]['id'] = node_id
@@ -116,33 +113,48 @@ def add_entity(network: Network, node_id: Union[str, int], connected: List[Any],
         for c in connected_nodes:
             network['graph'].nodes[c]['update'] = True
 
-    network['graph'].nodes[node_id]['update'] = False
+    network['graph'].nodes[node_id]['update'] = True
     return network
 
 def index_network(network: Network, depth: int) -> Network:
-    queue = [[n for n in network['graph'] if n['modified'] or 'vec' not in n], []]
+    if depth < 0:
+        raise ValueError(f"invalid update depth {depth}, depth must be > 0")
+    queue = [[n for n in network['graph'] if network['graph'].nodes[n]['update'] or 'vec' not in network['graph'].nodes[n]], []]
     ids = []
     vectors = []
     visited_set = set()
-    for _ in range(depth):
-        for node in queue[0]:
-            node_id = node['id']
+    for _ in range(depth + 1):
+        for node_id in queue[0]:
+            node = network['graph'].nodes[node_id]
             if node_id in visited_set:
                 continue
+            print(node)
 
             neighbors = [network['graph'][i] for i in nx.neighbors(network['graph'], node_id)]
             if node['update']:
                 network['graph'].nodes[node_id]['summary'] = get_entity_summary(network['client'], neighbors, 
                                                                                 'command-light', 500, 0.5, p=0.95)
-            vec = query_embed(network['client'], 'embed-english-light-v2.0', network['graph'][node_id]['summary'])
+                network['graph'].nodes[node_id]['update'] = False
+
+            vec = query_embed(network['client'], 'embed-english-light-v2.0', network['graph'].nodes[node_id]['summary']).squeeze()
+            print(vec)
             network['graph'].nodes[node_id]['vec'] = vec
-            vectors += vec
-            ids += [node_id]
+            vectors += [vec.tolist()]
+
+            if node_id not in network['id_map'].values():
+                # create new entry in id map
+                network['id_map'][hash(node_id)] = node_id
+            ids += [hash(node_id)]
+            print(ids)
             
             visited_set.add(node_id)
             queue[1] += neighbors
         queue = queue[1:]
     
-    ids = np.concatenate(ids)
-    vectors = np.vstack(vectors)
+    print(np.array(vectors).shape)
 
+    expr = f"id in {ids}"
+    network['milvus_info']['collection'].delete(expr)
+    network['milvus_info']['collection'].insert([ids, vectors]) 
+    network['milvus_info']['collection'].flush()
+    return network
