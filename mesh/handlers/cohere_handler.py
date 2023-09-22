65f1dcc631b572291ee7a392fcd47c956d754f18
Arnav Komaragiri <komaraac@mail.uc.edu>
2023-09-22 10:57:08 -0400

    added cohere chat functionality, built basic CLI skeleton

--- mesh/handlers/cohere_handler.py
+++ mesh/handlers/cohere_handler.py
@@ -1,3 +1,4 @@
+import json
 import cohere
 import numpy as np
 
@@ -18,6 +19,21 @@ def build_cot_prompt(prompt: str, rules: List[str], context_str: Optional[str] =
     return "Rules:\n{rule_str}\nTask: {prompt}\n{output}: ".format(prompt=prompt, rule_str=reduce(lambda a, b: f'{a}\n{b}', rules),
                                                                    output=output_name)
 
+def build_chat_prompt(chatbot_context: str, rules: List[str], prompt: str, example: Optional[str] = None) -> str:
+    output_str = ""
+    if example is not None:
+        output_str = "{chatbot_context}\nRules:\n{rule_str}\nExample Output:\n{example}".format(
+            chatbot_context=chatbot_context,
+            rule_str=reduce(lambda a, b: f'{a}\n{b}', rules),
+            example=example
+        )
+    else:
+        output_str = "{chatbot_context}\nRules:\n{rule_str}\nExample Output:\n{example}".format(
+            chatbot_context=chatbot_context,
+            rule_str=reduce(lambda a, b: f'{a}\n{b}', rules)
+        )
+    return output_str + f"\nUser Input: {prompt}\nResponse:"
+
 def query_generate(client: cohere.Client, model: str, num_tokens: int, temperature: float, p: float,
                    prompt: str, rules: List[str], context_str: str, output_name: str = "Response",
                    **kwargs) -> str:
@@ -34,6 +50,22 @@ def query_generate(client: cohere.Client, model: str, num_tokens: int, temperatu
     )
     # TODO: potentially integrate a reward model to do ranking of results from multiple traces
     return response.generations[0].text
+
+def query_chat(client: cohere.Client, model: str, question: str, chatbot_context: str, rules: List[str], example: Optional[str] = None) -> Dict[str, str]:
+    # build chat-specific prompt and run inference
+    chat_prompt = build_chat_prompt(chatbot_context, rules=[f"- {rule}" for rule in rules], prompt=question, example=example)
+    response = client.chat( 
+        model=model,
+        message=chat_prompt,
+        temperature=0.3,
+    ) 
+
+    # parse JSON response and return output
+    try:
+        output = json.loads(response.text)
+    except Exception as e:
+        raise ValueError(f"reached error when parsing JSON response:\nResponse:\n{response.txt}\nError:\n{e}")
+    return output
 
 def query_embed(client: cohere.Client, model: str, text: Union[str, List[str]]) -> np.array:
     # if we're embedding a single item, wrap it in a list
